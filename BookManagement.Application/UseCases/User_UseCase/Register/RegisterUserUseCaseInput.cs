dbb45f3deaeb5ba10bb7d45b6513c434eecd744e
Trần Minh Quân <minhquantb00@gmail.com>
2024-10-09 22:36:48 +0700

    add function register

---
+++ BookManagement.Application/UseCases/User_UseCase/Register/RegisterUserUseCaseInput.cs
@@ -0,0 +1,20 @@
+﻿using BookManagement.Commons.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagement.Application.UseCases.User_UseCase.Register
+{
+    public class RegisterUserUseCaseInput
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        [DataType(DataType.Upload)]
+        public Enumerate.Gender? Gender { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
