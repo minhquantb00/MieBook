4a4c0aef4253fa7ff81d0d854c91a34b598af272
Trần Minh Quân <minhquantb00@gmail.com>
2024-10-09 22:55:04 +0700

    update

--- BookManagement.Application/UseCases/User_UseCase/Register/RegisterUserUseCaseInput.cs
+++ BookManagement.Application/UseCases/User_UseCase/Register/RegisterUserUseCaseInput.cs
@@ -10,11 +10,19 @@ namespace BookManagement.Application.UseCases.User_UseCase.Register
 {
     public class RegisterUserUseCaseInput
     {
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "FullName is required")]
         public string FullName { get; set; }
+
         [DataType(DataType.Upload)]
         public Enumerate.Gender? Gender { get; set; }
+        
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "PhoneNumber is required")]
         public string PhoneNumber { get; set; }
     }
 }
